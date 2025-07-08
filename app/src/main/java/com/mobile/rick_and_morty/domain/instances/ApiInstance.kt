9555ef5d41173e68a4f7c6d56c316aa2960fb4c6
Maxim Karassev <maximus_1901@mail.ru>
2025-07-09 00:51:39 +0500

    deleted: instances package from domain

--- app/src/main/java/com/mobile/rick_and_morty/domain/instances/ApiInstance.kt
+++
@@ -1,7 +0,0 @@
-package com.mobile.rick_and_morty.domain.instances
-
-import com.mobile.rick_and_morty.data.remote.NetworkModule
-
-object ApiInstance {
-     val api = NetworkModule.provideApi()
-}
